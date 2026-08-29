8814396f5ec4cff6af59ddd47c8f964ee5e4c616
JuanMuneton87 <diego.muneton@udea.edu.co>
2026-08-28 20:51:21 -0500

    Actualiza menu de Cloud Computing

--- app/components/molecules/Menu/index.tsx
+++ app/components/molecules/Menu/index.tsx
@@ -2,10 +2,10 @@ import Navtitle from '@/app/components/atoms/Navtitle';
 const Index = () => {
   return (
     <div className='flex flex-col lg:flex-row items-center justify-around'>
-      <Navtitle text='Products' />
-      <Navtitle text='Solutions' />
-      <Navtitle text='Resources' />
-      <Navtitle text='Pricing' />
+      <Navtitle text='Taller' />
+      <Navtitle text='Practico' />
+      <Navtitle text='Cloud' />
+      <Navtitle text='Computing' />
     </div>
   );
 };
